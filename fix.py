ef001bbe239b299f04919acde9399c7a2b743e69
tetsushiwahaha <ueta@tokushima-u.ac.jp>
2022-08-09 11:28:12 +0900

    updated.

--- fix.py
+++ fix.py
@@ -15,58 +15,64 @@ class DataStruct():
 			print(f"Usage: python {sys.argv[0]} filename")
 			sys.exit(0)
 		fd = open(sys.argv[1], 'r')
-		self.dict = json.load(fd)
+		self.dic = json.load(fd)
 		fd.close()
-		self.dim = len(self.dict['x0'])
+		self.dim = len(self.dic['x0'])
 
 def func(t, x, data):
+	dim = data.dim
 	f = [ x[1], 
-		-data.dict['params'][0] * x[1] - x[0]**3 
-			+ data.dict['params'][1] + data.dict['params'][2] * cos(t)
+		-data.dic['params'][0] * x[1] - x[0]**3 
+			+ data.dic['params'][1] + data.dic['params'][2] * cos(t)
 	] 
 	p = -3.0 * x[0] * x[0]
-	k = data.dict['params'][0]
-	dim = data.dim
-	dphidx = x[dim:dim*dim+dim].reshape(dim, dim).transpose()
+	k = data.dic['params'][0]
+	cs = cos(t)
+	dphidx = x[dim:dim*dim+dim].reshape(dim, dim).T
+	dphidl = x[dim*dim:dim*dim+dim]
+	dfdl = [0, cs]
 	dfdx = [[0, 1], [p, -k]]
-	# return np.append(f, (dfdx @ dphidx).transpose().flatten())
-	f.extend((dfdx @ dphidx).transpose().flatten())
+	f.extend((dfdx @ dphidx).T.flatten())
+	f.extend(dfdx @ dphidl + dfdl)
 	return  f
 
 def fixed(data):
 	fperiod = 2*np.pi
-	duration = data.dict['period'] * fperiod
-	#tspan = np.arange(0, duration, data.dict['tick'])
-	x0 = data.dict['x0'] 
+	duration = data.dic['period'] * fperiod
+	#tspan = np.arange(0, duration, data.dic['tick'])
+	x0 = data.dic['x0'] 
 	dim = data.dim # number of dimension
 	count = 0
 	while True:
 		prev = x0 
-		data.x00 = np.append(x0, np.eye(dim).flatten())
+		xtemp = np.append(x0, np.eye(dim).flatten())
+		data.x00 = np.append(xtemp, [0, 0])
+		#print(data.x00)
 		x = solve_ivp(func, (0, duration), data.x00, 
 			#method = 'DOP853', 
 			method = 'RK45', 
 			rtol = 1e-8,
 			args = (data,) )	# pass a singleton
+		
 		vec = x.y[:,-1]		# copy last column
 		xs = vec[:dim]		# extract x
 		b = prev - xs
-		dphidx = vec[dim:].reshape(dim, dim).transpose()
+		dphidx = vec[dim:dim*dim+dim].reshape(dim, dim).T
 		A = dphidx - np.eye(dim)
 		dx = solve(A, b)		# solve Ax = b
 		delta = np.linalg.norm(dx, ord=2) 
-		if delta < data.dict['errors']:
+		if delta < data.dic['errors']:
 			break
-		if delta > data.dict['explosion']:
+		if delta > data.dic['explosion']:
 			print("exploded.")
 			sys.exit(-1)
-		if count >= data.dict['ite_max']:
+		if count >= data.dic['ite_max']:
 			print(f"over {count} times iteration")
 			sys.exit(-1)
 		x0 = prev + dx
 		count += 1
 	data.dphidx = dphidx
-	data.dict['x0'] = x0
+	data.dic['x0'] = x0
 	return count
 
 def main():
@@ -74,13 +80,13 @@ def main():
 	with open(logfilename, mode = 'w') as logfile:
 		while True:
 			iteration = fixed(data)
-			l = linalg.eig(data.dphidx.transpose())[0]
+			l = linalg.eig(data.dphidx.T)[0]
 			comp = True if abs(l[0].imag) < 1e-8 else False
 			str = "{0:2d} ".format(iteration)
-			for i in range(len(data.dict['params'])):
-				str += "{0: .5f} ".format(data.dict['params'][i])
-			for i in range(len(data.dict['x0'])):
-				str += "{0: .8f} ".format(data.dict['x0'][i])
+			for i in range(len(data.dic['params'])):
+				str += "{0: .5f} ".format(data.dic['params'][i])
+			for i in range(len(data.dic['x0'])):
+				str += "{0: .8f} ".format(data.dic['x0'][i])
 			if comp == True: 
 				str += "R "
 				str += "{0: .7f} {1: .7f}".format(l[0].real, l[1].real)
@@ -89,8 +95,8 @@ def main():
 				str += "{0: .7f} {1: .7f}".format(l[0].real, l[0].imag)
 			print(str)
 			logfile.write(str + "\n")
-			pos = data.dict['increment_param']
-			data.dict['params'][pos] += data.dict['dparams'][pos]
+			pos = data.dic['increment_param']
+			data.dic['params'][pos] += data.dic['dparams'][pos]
 
 if __name__ == '__main__':
 	main()
